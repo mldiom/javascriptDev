3c8ae18f5e86cf35f40125366563de4bd6443250
mldiom <mldiom@gmail.com>
2017-11-15 09:12:45 -0500

    Update and rename tempConversionApp to tempConversionApp.js

--- tempConversionApp.js
+++ tempConversionApp.js
@@ -1,7 +1,9 @@
 // current forecast of the day
 const kelvin = prompt('what is the kelvin temperature today?');
+
 // this converts kelvin into celsius
 const celsius = kelvin - 273;
+
 // conversion into farenheit, rounded to avoid decimals
 let fahrenheit = Math.floor(celsius * (9/5) + 32);
 
